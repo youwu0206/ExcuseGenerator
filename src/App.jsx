d7689e1a9dcea5e374bac1d2c4fd3b9194167788
You Wu <ywu34@uw.edu>
2025-05-19 16:13:47 -0700

    initial set up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Excuse Generator</h1>
+      <p>Let’s get you a great excuse!</p>
+    </div>
+  );
+}
+
+export default App;
